633041fa5aef785c451202097bfd845979a8b374
Nico Schlömer <nico.schloemer@gmail.com>
2020-06-17 23:18:02 +0200

    remove some unused code

--- krylov/givens.py
+++ krylov/givens.py
@@ -1,8 +1,6 @@
 import numpy
 import scipy.linalg.blas as blas
 
-from .errors import ArgumentError
-
 
 def givens(x):
     """Compute Givens rotation for provided vector x.
@@ -12,10 +10,6 @@ def givens(x):
     such that
     :math:`Gx=\\begin{bmatrix}r\\\\0\\end{bmatrix}`.
     """
-    # make sure that x is a vector ;)
-    if x.shape != (2,):
-        raise ArgumentError("x is not a vector of shape (2,)")
-
     if numpy.isreal(x).all():
         # real vector
         c, s = blas.drotg(*numpy.real(x))
