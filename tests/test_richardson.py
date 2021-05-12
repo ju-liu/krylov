7ef9e9e52a948f66d05ba974f5152413bbf7a74e
Nico Schlömer <nico.schloemer@gmail.com>
2021-05-12 23:46:52 +0200

    more richardson tests

--- tests/test_richardson.py
+++ tests/test_richardson.py
@@ -4,38 +4,50 @@ import pytest
 import krylov
 
 from .helpers import assert_consistent
-from .linear_problems import spd_dense, spd_rhs_0, spd_rhs_0sol0, spd_sparse
+from .linear_problems import (
+    complex_unsymmetric,
+    hermitian_indefinite,
+    hpd,
+    real_unsymmetric,
+    spd_dense,
+    spd_rhs_0,
+    spd_rhs_0sol0,
+    spd_sparse,
+    symmetric_indefinite,
+)
 
-# def test_reference():
-#     A, b = spd_dense((5,))
-#     _, info = krylov.richardson(A, b, tol=1.0e-5, maxiter=5)
-#     ref = np.array(
-#         [
-#             2.23606797749979,
-#             1.626826691029081,
-#             1.744954212067044,
-#             1.7113839589143471,
-#             1.6298632096913288,
-#             1.4593167230617032,
-#         ]
-#     )
-#     assert np.all(np.abs(info.resnorms - ref) < 1.0e-12 * (1.0 + ref))
+
+def test_reference():
+    A, b = spd_dense((5,))
+    _, info = krylov.richardson(A, b, tol=1.0e-5, maxiter=5)
+    ref = np.array(
+        [
+            2.23606797749979,
+            1.3620205578477882,
+            1.1590550073227759,
+            1.0655176233413504,
+            1.0133010995040146,
+            0.9806493287065431,
+        ]
+    )
+    print(info.resnorms)
+    assert np.all(np.abs(info.resnorms - ref) < 1.0e-12 * (1.0 + ref))
 
 
 @pytest.mark.parametrize(
     "A_b",
     [
         spd_dense((5,)),
-        # spd_sparse((5,)),
-        # spd_dense((5, 1)),
-        # spd_dense((5, 3)),
-        # spd_rhs_0((5,)),
-        # spd_rhs_0sol0(),
-        # hpd(),
-        # symmetric_indefinite(),
-        # hermitian_indefinite(),
-        # real_unsymmetric(),
-        # complex_unsymmetric(),
+        spd_sparse((5,)),
+        spd_dense((5, 1)),
+        spd_dense((5, 3)),
+        spd_rhs_0((5,)),
+        spd_rhs_0sol0(),
+        hpd(),
+        symmetric_indefinite(),
+        hermitian_indefinite(),
+        real_unsymmetric(),
+        complex_unsymmetric(),
     ],
 )
 def test_richardson(A_b):
