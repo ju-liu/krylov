edf5380b9fd4a8738b2db0e4eb98c5d41ef7732b
Nico Schlömer <nico.schloemer@gmail.com>
2021-05-16 14:30:58 +0200

    more symmlq tests

--- tests/test_symmlq.py
+++ tests/test_symmlq.py
@@ -15,6 +15,29 @@ from .linear_problems import (
 )
 
 
+def test_ref():
+    A, b = symmetric_indefinite()
+    _, info = krylov.symmlq(A, b)
+
+    ref = np.array(
+        [
+            2.23606797749979,
+            0.9823441352194251,
+            0.5792270481089666,
+            0.2307060320183781,
+            0.16833036914998076,
+            0.053350208455011996,
+            0.0033119344706411423,
+            0.27582030681696557,
+            0.2242220189485338,
+            0.07981010715785067,
+            4.440892098500626e-16,
+        ]
+    )
+    print(info.resnorms)
+    assert np.all(np.abs(info.resnorms - ref) < 1.0e-13 * (1.0 + np.abs(ref)))
+
+
 @pytest.mark.parametrize(
     "A_b",
     [
